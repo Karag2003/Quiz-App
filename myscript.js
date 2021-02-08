059d58125736eccef4ec0a6bfff7c62ca6de4f8e
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-02-08 09:58:25 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,17 @@
-
+$(document).ready(function(){
+   $("#a1").hide();
+   $("#a2").hide();
+   $("#a3").hide();
+   
+   $("#Hide").click(function(){
+    $("#a1").hide();
+    $("#a2").hide();
+    $("#a3").hide();
+   });
+   
+   $("#Show").click(function(){
+    $("#a1").show();
+    $("#a2").show();
+    $("#a3").show();
+   });
+});
